afc77d3edc562c8de5226e7d4421d92c23c0a5af
조주희 <carla_cho@naver.com>
2024-03-29 00:34:27 +0900

    TodoList 구현

---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import "./Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    };
+
+    // 엔터키 눌렀을 때 가능하게
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            onSubmit();
+        }
+    };
+
+    const onSubmit = () => {
+        if (content === "") {
+            contentRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        // 추가버튼 눌렀을 때 검색창 초기화(빈 문자열)
+        setContent("");
+    };
+
+    return (
+        <div className="Editor">
+            <input
+                ref={contentRef}
+                value={content}
+                onKeyDown={onKeyDown}
+                onChange={onChangeContent}
+                placeholder="새로운 Todo..."
+            />
+            <button onClick={onSubmit}>추가</button>
+        </div>
+    );
+};
+export default Editor;
